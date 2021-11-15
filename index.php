21802d414a2270578a315c850ab27a71abb9ed41
ZhRA0934 <92914417+ZhRA0934@users.noreply.github.com>
2021-11-15 21:11:28 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<form class="form-1">
+    <p class="field">
+        <input type="text" name="login" placeholder="Логин или емэйл">
+        <i class="icon-user icon-large"></i>
+    </p>
+        <p class="field">
+        <input type="password" name="password" placeholder="Пароль">
+        <i class="icon-lock icon-large"></i>
+    </p>      
+    <p class="submit">
+        <button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
+    </p>
+</form>
